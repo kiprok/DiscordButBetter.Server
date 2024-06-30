8e1782fbab6918977e1c122229f107a6e6467902
kiprok <quinn.vandenbroek@outlook.com>
2024-06-30 21:21:45 +0200

    added secret management

--- Modules/UsersModule.cs
+++ Modules/UsersModule.cs
@@ -7,34 +7,29 @@ public class UsersModule() : CarterModule("/api/users")
 {
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapGet("/", () =>
+        app.MapGet("/", (DbbContext db) =>
         {
-            return Results.Ok(new[]
-            {
-                new { Id = 1, Name = "Alice" },
-                new { Id = 2, Name = "Bob" },
-                new { Id = 3, Name = "Charlie" }
-            });
+            var users = db.Users.ToList();
+            return Results.Ok(users);
         });
         
         app.MapPut("/", (DbbContext db) =>
         {
-            for (int i = 0; i < 100; i++)
+            var user = new UserModel
             {
-                db.Users.Add(new UserModel
-                {
-                    Username = $"User{i}",
-                    Password = "password",
-                    CreatedAt = DateTime.Now,
-                    Status = 0,
-                    ProfilePicture = "https://i.imgur.com/Y86bvSa.jpeg",
-                    StatusMessage = "Hello, world!",
-                    Biography = "This is a user's biography."
-                });
-            }
+                Username = $"User{Random.Shared.Next(1000, 9999)}",
+                Password = "password",
+                CreatedAt = DateTime.Now,
+                Status = 0,
+                ProfilePicture = "https://i.imgur.com/Y86bvSa.jpeg",
+                StatusMessage = "Hello, world!",
+                Biography = "This is a user's biography."
+            };
+            
+            db.Users.Add(user);
             db.SaveChanges();
             
-            return Results.Ok();
+            return Results.Ok(user);
         });
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using Carter;
 using DiscordButBetter.Server.Database;
 using Microsoft.EntityFrameworkCore;
@@ -11,7 +12,19 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddCarter();
 
-var connectionString = $"server={Environment.GetEnvironmentVariable("DB_HOST", EnvironmentVariableTarget.User)};port={Environment.GetEnvironmentVariable("DB_PORT", EnvironmentVariableTarget.User)};database={Environment.GetEnvironmentVariable("DB_NAME", EnvironmentVariableTarget.User)};user={Environment.GetEnvironmentVariable("DB_USER", EnvironmentVariableTarget.User)};password={Environment.GetEnvironmentVariable("DB_PASS", EnvironmentVariableTarget.User)}";
+var db_host = Environment.GetEnvironmentVariable("DB_HOST") ?? builder.Configuration["DB_HOST"];
+var db_port = Environment.GetEnvironmentVariable("DB_PORT") ?? builder.Configuration["DB_PORT"];
+var db_name = Environment.GetEnvironmentVariable("DB_NAME") ?? builder.Configuration["DB_NAME"];
+var db_user = Environment.GetEnvironmentVariable("DB_USER") ?? builder.Configuration["DB_USER"];
+var db_pass = Environment.GetEnvironmentVariable("DB_PASS") ?? builder.Configuration["DB_PASS"];
+
+Console.WriteLine($"DB_HOST: {db_host}");
+Console.WriteLine($"DB_PORT: {db_port}");
+Console.WriteLine($"DB_NAME: {db_name}");
+Console.WriteLine($"DB_USER: {db_user}");
+Console.WriteLine($"DB_PASS: {db_pass}");
+
+var connectionString = $"Server={db_host};Port={db_port};Database={db_name};Uid={db_user};Pwd={db_pass};";
 var serverVersion = new MariaDbServerVersion(new Version(10, 11, 5));
 builder.Services.AddDbContext<DbbContext>(options =>
 {
