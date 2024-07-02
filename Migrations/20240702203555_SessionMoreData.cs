a178713f3dfe19666bbb9c97ea224a8d139434e6
kiprok <quinn.vandenbroek@outlook.com>
2024-07-02 22:43:29 +0200

    session info

---
+++ Migrations/20240702203555_SessionMoreData.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DiscordButBetter.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class SessionMoreData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "expiration",
+                table: "Sessions");
+
+            migrationBuilder.AddColumn<string>(
+                name: "IpAddress",
+                table: "Sessions",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "UserAgent",
+                table: "Sessions",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IpAddress",
+                table: "Sessions");
+
+            migrationBuilder.DropColumn(
+                name: "UserAgent",
+                table: "Sessions");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "expiration",
+                table: "Sessions",
+                type: "datetime(6)",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+    }
+}
