77cff0ac16420a63c862ae336aa2eb96b6835eb7
quinn <quinn.vandenBroek@outlook.com>
2024-07-02 19:31:29 +0200

    added random user api to registration

--- Modules/AuthenticationModule.cs
+++ Modules/AuthenticationModule.cs
@@ -1,4 +1,6 @@
-﻿using Carter;
+﻿using System.Text.Json;
+using System.Text.Json.Nodes;
+using Carter;
 using DiscordButBetter.Server.Authentication;
 using DiscordButBetter.Server.Contracts.Mappers;
 using DiscordButBetter.Server.Contracts.Requests;
@@ -6,6 +8,7 @@ using DiscordButBetter.Server.Database;
 using DiscordButBetter.Server.Database.Models;
 using DiscordButBetter.Server.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace DiscordButBetter.Server.Modules;
 
@@ -18,17 +21,43 @@ public class AuthenticationModule : CarterModule
     
     public override void AddRoutes(IEndpointRouteBuilder app)
     {
-        app.MapPost("/registration", async ([FromBody] RegistrationRequest request,DbbContext db, IUserService userService) =>
+        app.MapPost("/registration", async (
+            [FromBody] RegistrationRequest request,
+            DbbContext db,
+            IUserService userService,
+            HttpClient client) =>
         {
+            if (await db.Users.AnyAsync(u => u.Username == request.Username))
+            {
+                return Results.BadRequest("Username already exists.");
+            }
+
+            var randomProfilePicture = "";
+            var randomName = "";
+            var response = await client.GetAsync("https://randomuser.me/api/");
+            if(response.IsSuccessStatusCode)
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                var rUser = JsonNode.Parse(content);
+                randomProfilePicture = rUser["results"][0]["picture"]["large"].ToString();
+                randomName = rUser["results"][0]["name"]["first"].ToString();
+                randomName += " " + rUser["results"][0]["name"]["last"].ToString();
+            }
+            else
+            {
+                return Results.BadRequest("Failed to generate random user.");
+            }
+            
+            
             var user = new UserModel
             {
                 Username = request.Username,
                 Password = userService.GeneratePasswordHash(request.Password),
                 CreatedAt = DateTime.Now,
                 Status = 0,
-                ProfilePicture = "https://i.imgur.com/Y86bvSa.jpeg",
-                StatusMessage = "Hello, world!",
-                Biography = "This is a user's biography."
+                ProfilePicture = randomProfilePicture,
+                StatusMessage = $"My name is {randomName}!",
+                Biography = $"This is my very long biography. I am {randomName}.\n I am a new user.\n I am a very cool person."
             };
             
             db.Users.Add(user);
