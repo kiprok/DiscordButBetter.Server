bafcc81a34b02a15424b3df930f59aa30aec12ed
kiprok <quinn.vandenbroek@outlook.com>
2024-11-11 22:56:07 +0100

    WIP MessageService.cs

--- Modules/MessagesModule.cs
+++ Modules/MessagesModule.cs
@@ -29,7 +29,7 @@ public class MessagesModule : CarterModule
         app.MapGet("/conversation/{conversationId:guid}/older/{messageTime:Datetime}", GetOlderMessages);
 
         app.MapGet("/conversation/{conversationId:guid}/newer/{messageTime:Datetime}", GetNewerMessages);
-        
+
         app.MapGet("/conversation/{conversationId:guid}/point/{messageId:guid}", GetSurroundingMessages);
 
         app.MapGet("/{messageId:guid}", GetMessageById);
@@ -39,11 +39,11 @@ public class MessagesModule : CarterModule
         app.MapDelete("/{messageId:guid}", DeleteMessageById);
 
         app.MapPatch("/{messageId:guid}", UpdateMessageById);
-        
+
         app.MapGet("/conversation/{conversationId:guid}/search", SearchForMessages);
     }
 
-    private async Task<Results<Ok<MessageResponse>, NotFound,BadRequest<string>>> UpdateMessageById(
+    private async Task<Results<Ok<MessageResponse>, NotFound, BadRequest<string>>> UpdateMessageById(
         DbbContext db,
         Guid messageId,
         [FromBody] UpdateChatMessageRequest request,
@@ -52,21 +52,18 @@ public class MessagesModule : CarterModule
         var message = await db.Messages.FindAsync(messageId);
         if (message == null) return TypedResults.NotFound();
 
-        
-        if (request.Content.Length > 2000)
-        {
-            return TypedResults.BadRequest("Message content is too long.");
-        }
-        
+
+        if (request.Content.Length > 2000) return TypedResults.BadRequest("Message content is too long.");
+
         message.Content = request.Content;
         message.Metadata = request.Metadata.ToString();
 
         await db.SaveChangesAsync();
 
         var response = message.ToMessageResponse();
-        
+
         await bus.Publish(message.ToEditChatMessageMessage());
-        
+
         return TypedResults.Ok(response);
     }
 
@@ -80,118 +77,171 @@ public class MessagesModule : CarterModule
 
         db.Messages.Remove(message);
         await db.SaveChangesAsync();
-        
+
         await bus.Publish(message.ToDeleteChatMessageMessage());
-        
+
         return TypedResults.Ok();
     }
 
-    private async Task<Results<Ok<MessageResponse>,BadRequest<string>>> CreateNewMessage(
-        DbbContext db, 
+    private async Task<Results<Ok<MessageResponse>, NotFound, BadRequest, UnauthorizedHttpResult>> CreateNewMessage(
+        IMessageService messageService,
+        IConversationService conversationService,
         [FromBody] SendChatMessageRequest request,
         ClaimsPrincipal claim,
         IBus bus)
     {
-        if (request.Content.Length > 2000)
-        {
-            return TypedResults.BadRequest("Message content is too long.");
-        }
-        
+        if (request.Content.Length > 2000) return TypedResults.BadRequest();
         var userId = Guid.Parse(claim.Claims.First().Value);
+
+        var conversation = await conversationService.GetConversationById(request.ConversationId);
+
+        if (conversation == null) return TypedResults.NotFound();
+
+        if (conversation.Participants.FirstOrDefault(x => x.Id == userId) == null)
+            return TypedResults.Unauthorized();
+
         var message = request.ToChatMessageModel();
-        message.Id = Guid.NewGuid();
         message.SenderId = userId;
-        var conversation = await db.Conversations.FindAsync(message.ConversationId);
-        conversation!.LastMessageTime = message.SentAt;
+        var result = await messageService.CreateNewMessage(message, conversation);
 
-        db.Messages.Add(message);
-        await db.SaveChangesAsync();
+        var response = result.ToMessageResponse();
 
-        var response = message.ToMessageResponse();
+        await bus.Publish(result.ToSendChatMessageMessage());
 
-        await bus.Publish(message.ToSendChatMessageMessage());
-        
         return TypedResults.Ok(response);
     }
 
-    private async Task<Results<Ok<MessageResponse>, NotFound>> GetMessageById(DbbContext db, Guid messageId)
+    private async Task<Results<Ok<MessageResponse>, NotFound>> GetMessageById(
+        IMessageService messageService,
+        IConversationService conversationService,
+        ClaimsPrincipal claim,
+        Guid messageId)
     {
-        var message = await db.Messages.FindAsync(messageId);
+        var userId = Guid.Parse(claim.Claims.First().Value);
+
+        var message = await messageService.GetMessageById(messageId);
         if (message == null) return TypedResults.NotFound();
+
+        var conversation = await conversationService.GetConversationById(message.ConversationId);
+
+        if (conversation?.Participants.FirstOrDefault(x => x.Id == userId) == null)
+            return TypedResults.NotFound();
 
         return TypedResults.Ok(message.ToMessageResponse());
     }
 
-    private Ok<List<MessageResponse>> GetNewerMessages(DbbContext db, Guid conversationId,
+    private async Task<Results<Ok<List<MessageResponse>>, NotFound, UnauthorizedHttpResult>> GetNewerMessages(
+        IConversationService conversationService,
+        IMessageService messageService,
+        ClaimsPrincipal claim,
+        Guid conversationId,
         DateTime messageTime)
     {
-        var messages = db.Messages
-            .Where(m => m.ConversationId == conversationId && m.SentAt > messageTime)
-            .OrderBy(m => m.SentAt)
-            .Take(50);
+        var userId = Guid.Parse(claim.Claims.First().Value);
+
+        var conversation = await conversationService.GetConversationById(conversationId);
+
+        if (conversation == null) return TypedResults.NotFound();
+
+        if (conversation.Participants.FirstOrDefault(x => x.Id == userId) == null)
+            return TypedResults.Unauthorized();
+
+        var messages = await messageService.GetNewerMessages(conversationId, 50, messageTime);
+
         return TypedResults.Ok(messages.Select(m => m.ToMessageResponse()).ToList());
     }
 
-    private Ok<List<MessageResponse>> GetOlderMessages(DbbContext db, Guid conversationId,
+    private async Task<Results<Ok<List<MessageResponse>>, NotFound, UnauthorizedHttpResult>> GetOlderMessages(
+        IConversationService conversationService,
+        IMessageService messageService,
+        ClaimsPrincipal claim,
+        Guid conversationId,
         DateTime messageTime)
     {
-        var messages = db.Messages
-            .Where(m => m.ConversationId == conversationId && m.SentAt < messageTime)
-            .OrderByDescending(m => m.SentAt)
-            .Take(50);
+        var userId = Guid.Parse(claim.Claims.First().Value);
+
+        var conversation = await conversationService.GetConversationById(conversationId);
+
+        if (conversation == null) return TypedResults.NotFound();
+
+        if (conversation.Participants.FirstOrDefault(x => x.Id == userId) == null)
+            return TypedResults.Unauthorized();
+
+        var messages = await messageService.GetOlderMessages(conversationId, 50, messageTime);
+
         return TypedResults.Ok(messages.Select(m => m.ToMessageResponse()).ToList());
     }
 
-    private Ok<List<MessageResponse>> GetMessagesFromConversation(DbbContext db, Guid conversationId)
+    private async Task<Results<Ok<List<MessageResponse>>, NotFound, UnauthorizedHttpResult>>
+        GetMessagesFromConversation(
+            IConversationService conversationService,
+            IMessageService messageService,
+            ClaimsPrincipal claim,
+            Guid conversationId)
     {
-        var messages = db.Messages
-            .Where(m => m.ConversationId == conversationId)
-            .OrderByDescending(m => m.SentAt)
-            .Take(50);
+        var userId = Guid.Parse(claim.Claims.First().Value);
+
+        var conversation = await conversationService.GetConversationById(conversationId);
+
+        if (conversation == null) return TypedResults.NotFound();
+
+        if (conversation.Participants.FirstOrDefault(x => x.Id == userId) == null)
+            return TypedResults.Unauthorized();
+
+        var messages = await messageService.GetMessagesFromConversation(conversationId);
+
         return TypedResults.Ok(messages.Select(m => m.ToMessageResponse()).ToList());
     }
-    
-    private Results<Ok<List<MessageResponse>>,NotFound> GetSurroundingMessages(DbbContext db, Guid conversationId, Guid messageId)
+
+    private async Task<Results<Ok<List<MessageResponse>>, NotFound, UnauthorizedHttpResult>> GetSurroundingMessages(
+        IConversationService conversationService,
+        IMessageService messageService,
+        ClaimsPrincipal claim,
+        Guid conversationId,
+        Guid messageId)
     {
-        var targetMessage = db.Messages.FirstOrDefault(m => m.Id == messageId);
-        if(targetMessage == null) return TypedResults.NotFound();
-        
-        var messagesAbove = db.Messages
-            .Where(m => m.ConversationId == conversationId && m.SentAt < targetMessage.SentAt)
-            .OrderByDescending(m => m.SentAt)
-            .Take(25).ToList();
-        
-        var messagesBelow = db.Messages
-            .Where(m => m.ConversationId == conversationId && m.SentAt > targetMessage.SentAt)
-            .OrderBy(m => m.SentAt)
-            .Take(25).ToList();
-        
+        var userId = Guid.Parse(claim.Claims.First().Value);
+
+        var conversation = await conversationService.GetConversationById(conversationId);
+
+        if (conversation == null) return TypedResults.NotFound();
+
+        if (conversation.Participants.FirstOrDefault(x => x.Id == userId) == null)
+            return TypedResults.Unauthorized();
+
+        var targetMessage = await messageService.GetMessageById(messageId);
+        if (targetMessage == null) return TypedResults.NotFound();
+
+        var messagesAbove = await messageService.GetOlderMessages(conversationId, 25, targetMessage.SentAt);
+
+        var messagesBelow = await messageService.GetNewerMessages(conversationId, 25, targetMessage.SentAt);
+
         var combinedMessages = messagesAbove
-            .Concat(new []{targetMessage})
+            .Concat(new[] { targetMessage })
             .Concat(messagesBelow)
             .OrderByDescending(m => m.SentAt);
-        
+
         return TypedResults.Ok(combinedMessages.Select(m => m.ToMessageResponse()).ToList());
     }
-    
+
     private Ok<MessageSearchResponse> SearchForMessages(
-        DbbContext db, 
-        Guid conversationId, 
-        [FromQuery]string query, 
-        [FromQuery]int page = 1)
+        DbbContext db,
+        Guid conversationId,
+        [FromQuery] string query,
+        [FromQuery] int page = 1)
     {
         var messages = db.Messages
             .Where(m => m.ConversationId == conversationId)
             .Where(m => m.Content.Contains(query))
             .OrderByDescending(m => m.SentAt)
             .ToList();
-        
-        
+
+
         return TypedResults.Ok(new MessageSearchResponse
         {
             TotalCount = messages.Count,
             Messages = messages
-                .Skip((page-1) * 25)
+                .Skip((page - 1) * 25)
                 .Take(25)
                 .Select(m => m.ToMessageResponse()).ToList()
         });
---
+++ Services/MessageService.cs
@@ -0,0 +1,75 @@
+﻿using DiscordButBetter.Server.Database;
+using DiscordButBetter.Server.Database.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscordButBetter.Server.Services;
+
+public interface IMessageService
+{
+    public Task<ChatMessageModel?> GetMessageById(Guid messageId);
+    public Task<List<ChatMessageModel>> GetMessagesFromConversation(Guid conversationId);
+    public Task<List<ChatMessageModel>> GetOlderMessages(Guid conversationId, int total, DateTime messageTime);
+    public Task<List<ChatMessageModel>> GetNewerMessages(Guid conversationId, int total, DateTime messageTime);
+    public Task<ChatMessageModel> CreateNewMessage(ChatMessageModel message, ConversationModel conversation);
+    public Task<bool> DeleteMessageById(Guid messageId);
+    public Task<bool> UpdateMessageById(Guid messageId, string content, string metadata);
+    public Task<List<ChatMessageModel>?> SearchForMessages(Guid conversationId, string query);
+}
+
+public class MessageService(DbbContext db) : IMessageService
+{
+    public async Task<ChatMessageModel?> GetMessageById(Guid messageId)
+    {
+        return await db.Messages.FindAsync(messageId);
+    }
+
+    public async Task<List<ChatMessageModel>> GetMessagesFromConversation(Guid conversationId)
+    {
+        return await db.Messages
+            .Where(m => m.ConversationId == conversationId)
+            .OrderByDescending(m => m.SentAt)
+            .Take(50)
+            .ToListAsync();
+    }
+
+    public async Task<List<ChatMessageModel>> GetOlderMessages(Guid conversationId, int total, DateTime messageTime)
+    {
+        return await db.Messages
+            .Where(m => m.ConversationId == conversationId && m.SentAt < messageTime)
+            .OrderByDescending(m => m.SentAt)
+            .Take(total)
+            .ToListAsync();
+    }
+
+    public async Task<List<ChatMessageModel>> GetNewerMessages(Guid conversationId, int total, DateTime messageTime)
+    {
+        return await db.Messages
+            .Where(m => m.ConversationId == conversationId && m.SentAt > messageTime)
+            .OrderBy(m => m.SentAt)
+            .Take(total)
+            .ToListAsync();
+    }
+
+    public async Task<ChatMessageModel> CreateNewMessage(ChatMessageModel message, ConversationModel conversation)
+    {
+        db.Messages.Add(message);
+        conversation.LastMessageTime = message.SentAt;
+        await db.SaveChangesAsync();
+        return message;
+    }
+
+    public Task<bool> DeleteMessageById(Guid messageId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<bool> UpdateMessageById(Guid messageId, string content, string metadata)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<ChatMessageModel>?> SearchForMessages(Guid conversationId, string query)
+    {
+        throw new NotImplementedException();
+    }
+}
